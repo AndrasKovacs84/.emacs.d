76186449094d66dffb9886ccd91f0998f0f59b29
apha <ka841001@gmail.com>
2018-11-27 06:08:57 +0100

    working on ccls and lsp

--- init.el
+++ init.el
@@ -26,11 +26,10 @@
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (evil-commentary ibuffer-sidebar vscode-icon dired-sidebar evil-leader yasnippet-snippets yasnippet company-irony swiper popup-kill-ring exwm symon dmenu spaceline company dashboard rainbow-delimiters evil sudo-edit winum smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package))))
+    (lsp-ui company-lsp ccls evil-commentary ibuffer-sidebar vscode-icon dired-sidebar evil-leader yasnippet-snippets yasnippet company-irony swiper popup-kill-ring exwm symon dmenu spaceline company dashboard rainbow-delimiters evil sudo-edit winum smex ido-vertical-mode org-bullets beacon spacemacs-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;;'(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "ADBO" :family "Source Code Pro")))))
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "ADBO" :family "Source Code Pro Medium")))))
